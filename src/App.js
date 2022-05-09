da1df4030856365c0766459951572d3adfc0f083
RafiMahmudRizvi <89313047+RafiMahmudRizvi@users.noreply.github.com>
2022-05-09 16:25:07 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import JSONdata from './MOCK_DATA.json'
 
 function App() {
+  const [searchItem, setSearchItem] = useState("")
+
+  function handelChange(e){
+    setSearchItem(e.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <input type="text" placeholder='Search...' onChange={handelChange}/>
+       <div className="display_grid">
+            {JSONdata.filter((val)=>{
+              if (searchItem === ""){
+                return val
+              }else if (val.first_name.toLowerCase().includes(searchItem.toLowerCase())){
+                return val
+              }
+            }).map((val, key)=>{
+              return(
+                <div className='item' key={val.id}>
+                  <h5>{val.first_name}</h5>
+                  <h6>{val.last_name}</h6>
+                  <p>{val.email}</p>
+                  <p>{val.gender}</p>
+                  <p>{val.ip_address}</p>
+                </div>
+              )
+            })}
+                
+
+       </div>
     </div>
   );
 }
